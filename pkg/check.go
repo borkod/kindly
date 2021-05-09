b0820aaa09140a05f9fed33ff9996e2fb582c867
Borko <borkod@gmail.com>
2021-05-09 16:52:44 -0400

    Fixed check command.

--- pkg/check.go
+++ pkg/check.go
@@ -9,32 +9,36 @@ import (
 )
 
 // Check function checks if the packages passed in args are available TODO variadic function
-func (k Kindly) Check(ctx context.Context, s string, n string) (ks KindlyStruct, err error) {
+func (k Kindly) Check(ctx context.Context, s string, n string) (x bool, ks KindlyStruct, err error) {
 	client := github.NewClient(nil)
 	sources := k.cfg.Sources
-	println(s)
+	x = false
+
 	if len(s) > 0 {
 		sources = make(map[string]config.Source)
 		sources[s] = k.cfg.Sources[s]
 	}
 
 	for key, source := range sources {
-		println(key)
 		if source.Type == "github" {
 			_, dir, _, err := client.Repositories.GetContents(ctx, source.Owner, source.Repo, source.Path, nil)
 			if err != nil {
-				return ks, err
+				return false, ks, err
 			}
 
 			for _, n := range dir {
 				_, ks, err = k.getValidYConfig(ctx, key, strings.TrimSuffix(*n.Name, ".yaml"), false, false)
 				if err != nil {
-					return ks, err
+					return false, ks, err
 				}
 
 			}
 		}
 	}
 
-	return ks, nil
+	if len(ks.Spec.Name) > 0 {
+		x = true
+	}
+
+	return x, ks, nil
 }
